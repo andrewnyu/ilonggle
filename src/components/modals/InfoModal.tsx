d5d201c74ec4d7ff1ee50b8939d63ae4870afde7
Andrew Yu <andrewthomasyu@gmail.com>
2022-04-09 17:46:28 +0800

    Update content from Ilonggle V1 1.) Ilonggo language words list and valid guesses 2.) Ilonggo language Info Modal

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,61 +10,71 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+      Guess the ILONGGLE in 6 tries.
+      Each guess must be a valid 5 letter word. Hit the enter button to submit.
+      After each guess, the color of the tiles will change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="H"
           status="correct"
         />
-        <Cell value="E" />
+        <Cell value="U" />
+        <Cell value="L" />
         <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter H is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="K" />
+        <Cell value="A" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="M"
           status="present"
         />
         <Cell value="O" />
         <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter M is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
+        <Cell value="K" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="D" />
+        <Cell isRevealing={true} isCompleted={true} value="T" status="absent" />
+        <Cell value="O" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter T is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        Cloned from this {' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          open source repository
+        </a>{' '}
+      </p>
+
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        Words obtained from {' '}
+        <a
+          href="https://hiligaynon.pinoydictionary.com/"
+          className="underline font-bold"
+        >
+          Hiligaynon Dictionary
         </a>{' '}
       </p>
     </BaseModal>
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,8 +1,8 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = ['Sagad', 'Grabe, hinay hinay lang da', 'Mayo ah']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Kulang letra'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
